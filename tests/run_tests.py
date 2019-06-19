57b88ac0d0f64ccc8c00210a2f66ce6efefaf29b
Amr Keleg <amr_mohamed@live.com>
2019-06-19 22:43:05 +0200

    lt-weight: Use HFST to weight the input fst

---
+++ tests/run_tests.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+sys.path.append(os.path.realpath("."))
+
+import unittest
+import lt_proc, lt_trim, lt_print
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(__file__))
+    failures = 0
+    for module in [lt_trim, lt_proc, lt_print]:
+        suite = unittest.TestLoader().loadTestsFromModule(module)
+        res = unittest.TextTestRunner(verbosity = 2).run(suite)
+        failures += len(res.failures)
+    sys.exit(min(failures, 255))
